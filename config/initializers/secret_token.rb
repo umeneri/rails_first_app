777e6cb720dde1053246bc652e7054cde0379431
umeneri <mokushiroku97@gmail.com>
2015-09-01 22:55:34 +0900

    reconstructure repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = '33004245bd990caa77a8d15c420ee77a6bb69ba443cf2240585818f46feef0cb654ad5f5e7e03844d29dd94a621be9ff0257cf320fefed145b188673f34dc6d1'
